4172fda35133a28d22769c780712e712677a8f18
AleksandraTE22C <aleksandra.kajak-tonkiel@elev.ga.ntig.se>
2024-10-02 15:01:27 +0200

    Tested the life status

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Tamagotchi!");
+        Thread.sleep(1000);
         Scanner tb = new Scanner(System.in);
         System.out.println("Name your new virtual pet:");
-        tamagotchi pet = new tamagotchi(tb.next());
+        Tamagotchi pet = new Tamagotchi(tb.nextLine());
+        Thread.sleep(1000);
         while (pet.getAlive()){
+            System.out.println("Your pet's name is " + pet.name);
+            Thread.sleep(1000);
 
+            pet.setAlive();
         }
     }
+
 }
---
+++ src/Tamagotchi.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class Tamagotchi {
+    private int hunger = 0;
+    private int boredom = 0;
+    private boolean isAlive;
+
+    private Random generator = new Random();
+    String name;
+
+    public Tamagotchi(String incomingName) {
+        name = incomingName;
+        isAlive = true;
+        generator.nextInt(10);
+    }
+
+    public boolean getAlive() {
+        return isAlive;
+    }
+
+    public void setAlive() throws InterruptedException {
+        if (hunger >= 10) {
+            isAlive = false;
+            System.out.println("You neglected to take care of your pet.");
+            Thread.sleep(1500);
+            System.out.println(name + " has died.");
+            Thread.sleep(1000);
+            System.out.println("It has moved on to a place that knows no boredom, where it's belly will always be full and it's coat always clean and fluffy.");
+            Thread.sleep(2000);
+            System.out.println("R.I.P.");
+        } else if (boredom >= 10) {
+            isAlive = false;
+            System.out.println("You neglected to take care of your pet.");
+            Thread.sleep(1500);
+            System.out.println(name + " ran away. It was eventually found and adopted by a family that loved and cherished it.");
+            Thread.sleep(1000);
+            System.out.println("Maybe if you spent more time with it...");
+        } else {
+            isAlive = true;
+        }
+    }
+
+}
